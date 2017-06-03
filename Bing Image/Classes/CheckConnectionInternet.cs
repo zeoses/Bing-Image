d153cfcd55d801d0a62af62add0724fc04ac7e23
zeoses <zeoses@gmail.com>
2017-06-03 10:11:30 +0430

    ایجاد پروژه و کلاس های مربوطه

---
+++ Bing Image/Classes/CheckConnectionInternet.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bing_Image.Classes
+{
+    public static class CheckConnectionInternet
+    {
+
+        public static bool CheckForPinck()
+        {
+            try 
+            { 
+
+                    Ping myPing = new Ping();
+                    String host = "google.com";
+                    byte[] buffer = new byte[32];
+                    int timeout = 1000;
+                    PingOptions pingOptions = new PingOptions();
+                    PingReply reply = myPing.Send(host, timeout, buffer, pingOptions);
+                    return (reply.Status == IPStatus.Success);
+
+            }
+
+            catch (Exception)
+            {
+                 return false;
+            }
+        }
+
+        public static bool CheckForNetwork()
+        {
+            try
+            {
+
+                using (var client = new WebClient())
+                {
+                    using (var stream = client.OpenRead("http://www.google.com"))
+                    {
+                        return true;
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
